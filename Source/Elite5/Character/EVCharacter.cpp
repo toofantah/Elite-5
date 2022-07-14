5f1ccd01ed5a65d86c13b96bd3a872844f29f6b2
toofantah <toofan.tah@gmail.com>
2022-07-14 13:48:05 +0300

    Camera Component
    Added Camera Component
    Added SpringArmComponent

--- Source/Elite5/Character/EVCharacter.cpp
+++ Source/Elite5/Character/EVCharacter.cpp
@@ -2,6 +2,9 @@
 
 
 #include "EVCharacter.h"
+#include "GameFrameWork/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
 
 // Sets default values
 AEVCharacter::AEVCharacter()
@@ -9,23 +12,31 @@ AEVCharacter::AEVCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CameraBoom = CreateAbstractDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetMesh());
+	CameraBoom->TargetArmLength = 600.f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
 }
 
-// Called when the game starts or when spawned
+
 void AEVCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
+
 void AEVCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
+
 void AEVCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
